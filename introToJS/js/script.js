18607c6bb8624dca721910443908c68dcfecd9ef
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-05 20:14:43 -0800

    defining function within function

--- introToJS/js/script.js
+++ introToJS/js/script.js
@@ -5,11 +5,14 @@ console.log("global: message = " + message);
 var a = function() {
     var message = "inside a";
     console.log("a: message = " + message);
+
+    function b() {
+        console.log("b: message = " + message);
+    }
+
     b();
 }
 
-function b() {
-    console.log("b: message = " + message);
-}
+
 
 a();
