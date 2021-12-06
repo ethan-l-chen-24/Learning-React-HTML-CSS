6047d1ab3f453917d87defe195b4567606893c5e
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-06 15:07:15 -0800

    learning js object properties

--- introToJS/js/object.js
+++ introToJS/js/object.js
@@ -3,5 +3,12 @@ company.name = "Facebook";
 company.ceo = new Object();
 company.ceo.firstName = "Mark";
 company.ceo.lastName = "Zuckerberg";
+company["stock of company"] = 110;
+
+var competitor = "competitor";
+company[competitor] = "Amazon";
 
 console.log(company);
+console.log("Company name is " + company.name);
+console.log("Stock price is " + company["stock of company"]);
+console.log("Competitor is " + company[competitor]);
