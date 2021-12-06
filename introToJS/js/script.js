37407fdc0bd275d57d99e0999abecef05879df6e
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-06 14:44:09 -0800

    learning types

--- introToJS/js/script.js
+++ introToJS/js/script.js
@@ -9,10 +9,24 @@ var a = function() {
     function b() {
         console.log("b: message = " + message);
     }
-
     b();
 }
-
-
-
 a();
+
+var y;
+if(y == undefined) {
+    console.log("y is undefined");
+}
+
+y = 5;
+if(y == "5") {
+    console.log("5 == '5'");
+} else {
+    console.log("5 != '5'");
+}
+
+if(y === "5") {
+    console.log("5 strict equals '5'");
+} else {
+    console.log("5 does not strict equal '5'");
+}
