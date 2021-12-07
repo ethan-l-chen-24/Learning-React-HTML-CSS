80d337423911a2b20ae08085af858699aba6c173
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-06 19:52:28 -0800

    learned array shorthand

--- introToJS/js/arrays.js
+++ introToJS/js/arrays.js
@@ -7,3 +7,13 @@ array[2] = function(name) {
 array[3] = {course: "HTML, CSS & JS"};
 
 console.log(array);
+console.log(array[2]);
+array[2](array[0]);
+
+// short hand array creation
+var names = ["Ethan", "John", "JPak"];
+var random = ["Ethan", 2, {name: "Jason", age: 18}];
+
+for (var i = 0; i < names.length; i++) {
+    console.log("Hello " + names[i]);
+}
