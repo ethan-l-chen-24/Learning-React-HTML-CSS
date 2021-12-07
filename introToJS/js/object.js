8ae8930657caed933a41ee19475d73acc5c72371
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-06 17:46:04 -0800

    learning function constructors

--- introToJS/js/object.js
+++ introToJS/js/object.js
@@ -36,3 +36,18 @@ b.x = 5;
 console.log("after b.x update: ");
 console.log(a);
 console.log(b);
+
+// Function constructor
+function test() {
+    console.log(this);
+    this.myName = "Yaakov";
+}
+test();
+console.log(window.myName);
+
+function Circle (radius) {
+    this.radius = radius;
+    
+}
+
+var myCircle = new Circle(10);
