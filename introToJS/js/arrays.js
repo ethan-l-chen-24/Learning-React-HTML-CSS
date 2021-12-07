3e8f7cd1c411be13f7d89dbaf333eeb353f1ff71
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-06 20:01:02 -0800

    learning about for in loop for js

--- introToJS/js/arrays.js
+++ introToJS/js/arrays.js
@@ -17,3 +17,22 @@ var random = ["Ethan", 2, {name: "Jason", age: 18}];
 for (var i = 0; i < names.length; i++) {
     console.log("Hello " + names[i]);
 }
+
+var names2 = ["Yaakov", "John", "Joe"];
+
+var myObj = {
+    name: "Yaakov",
+    course: "HTML/CSS/JS",
+    platform: "Coursera"
+};
+
+for(var prop in myObj) {
+    console.log(prop + ": " + myObj[prop]);
+}
+
+// loops over properties of array, so if I add a new one it will still be included!
+names2.newProp = "random";
+for (var index in names2) {
+    console.log("Hello " + names2[name]);
+}
+
