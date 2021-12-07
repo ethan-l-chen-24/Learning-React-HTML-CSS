d13bef08ca04ea9403ffa9f463625fce503a3d70
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-06 20:23:42 -0800

    learned IIFE and fake namespace

---
+++ introToJS/js/miscjs.js
@@ -0,0 +1,9 @@
+(function(window) {
+    var ethanGreeter = {};
+    ethanGreeter.name = "ethan";
+    ethanGreeter.sayHi = function(name) {
+        console.log("hi " + ethanGreeter.name);
+    }
+
+    window.ethanGreeter = ethanGreeter;
+})(window);
