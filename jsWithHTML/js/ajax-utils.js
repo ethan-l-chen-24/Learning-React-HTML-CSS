61dbc283776e876d16318da13c4ee4284037182e
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-08 12:30:37 -0800

    made ajax get

--- jsWithHTML/js/ajax-utils.js
+++ jsWithHTML/js/ajax-utils.js
@@ -17,4 +17,23 @@
         }
     }
 
+    ajaxUtils.sendGetRequest = 
+        function(requestUrl, responseHandler) {
+            var request = getRequestObject();
+            request.onreadystatechange = 
+                function() {
+                    handleResponse(request, responseHandler);
+                };
+            request.open("GET", requestURL, true); // true denotes asynchronous
+            request.send(null);
+        };
+
+    function handleResponse(request, responseHandler) {
+        if((request.readyState == 4) && (request.status == 200)) {
+            responseHandler(request);
+        }
+    }
+
+    globalThis.$ajaxUtils = ajaxUtils;
+
 })(window);
