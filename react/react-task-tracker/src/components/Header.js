26d7e65133fc2b79c4e8ae214ef10af4d9383987
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-11 14:24:10 -0800

    added tasks component

--- react/react-task-tracker/src/components/Header.js
+++ react/react-task-tracker/src/components/Header.js
@@ -2,20 +2,22 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = function(event) {
+        console.log('click');
+    }
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Hello' />
-            <Button color='blue' text='Hello 1' />
-            <Button color='red' text='Hello 3' />
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
 
 // CSS in JS
-const headerStyle = {
+/*const headerStyle = {
     color: 'red'
-}
+}*/
 
 Header.defaultProps = {
     title: "Task Tracker"
