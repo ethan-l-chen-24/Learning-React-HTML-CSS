82b2c6025edf1b8292af9f86c470e634f0200da9
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-11 13:37:42 -0800

    adding props to a component

--- react/react-task-tracker/src/App.js
+++ react/react-task-tracker/src/App.js
@@ -1,9 +1,9 @@
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="container">
-      <div> this is my first app </div>
-      <div> woah I can put two things in this </div>
+      <Header title='Hello' />
     </div>
   );
 }
