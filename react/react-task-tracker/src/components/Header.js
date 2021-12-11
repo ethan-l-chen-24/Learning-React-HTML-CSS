e06a755a1f239c274a13dba1aa22e86d7a89ce25
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-11 14:05:38 -0800

    added more buttons

--- react/react-task-tracker/src/components/Header.js
+++ react/react-task-tracker/src/components/Header.js
@@ -1,9 +1,28 @@
-const Header = (props) => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
     return (
-        <header>
-            <h1>{props.title}</h1>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Hello' />
+            <Button color='blue' text='Hello 1' />
+            <Button color='red' text='Hello 3' />
         </header>
     )
+}
+
+// CSS in JS
+const headerStyle = {
+    color: 'red'
+}
+
+Header.defaultProps = {
+    title: "Task Tracker"
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
 }
 
 export default Header;
