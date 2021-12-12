e5b5719e2502cf2e92cc7db8003b9527d743b2d5
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-11 16:51:07 -0800

    passing delete down from app

--- react/react-task-tracker/src/App.js
+++ react/react-task-tracker/src/App.js
@@ -1,11 +1,41 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App() {
+
+  // first is the name of the state, the second is the function
+    // to update the state
+    const [tasks, setTasks] = useState([
+      {
+          id: 1,
+          text: "Doctors Appointment",
+          day: "Feb 5th at 2:30pm",
+          reminder: true
+      },
+      {
+          id: 2,
+          text: "Meeting at School",
+          day: "Feb 6th at 1:30pm",
+          reminder: true
+      },
+      {
+          id: 3,
+          text: "Food Shopping",
+          day: "Feb 5th at 2:30pm",
+          reminder: false
+      }
+  ]);
+
+  // delete task
+  const deleteTask = (id) => {
+    console.log('delete', id)
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
